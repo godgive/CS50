32867e58cc3472dd20d8a57787aef8a7a4180906
Dieudonne Sogbossi <rickrozay32@gmail.com>
2018-02-11 03:19:55 -0500

    Update records.php

--- records.php
+++ records.php
@@ -10,27 +10,29 @@ include("connect-db.php");
 // since this form is used multiple times in this file, I have made it a function that is easily reusable
 function renderForm($first = '', $last ='', $pos = '', $shirt = '', $error = '', $id = '')
 { ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-<title>
-<?php if ($id != '') { echo "Edit Record"; } else { echo "New Record"; } ?>
-</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-</head>
-<body>
-<h1><?php if ($id != '') { echo "Edit Record"; } else { echo "New Record"; } ?></h1>
-<?php if ($error != '') {
-echo "<div style='padding:4px; border:1px solid red; color:red'>" . $error
-. "</div>";
-} ?>
+    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+    <html>
+      <head>
+        <title>
+          <?php if ($id != '') { echo "Edit Record"; } else { echo "New Record"; } ?>
+        </title>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+      </head>
+          <body>
+             <h1><?php if ($id != '') { echo "Edit Record"; } else { echo "New Record"; } ?></h1>
+             <?php if ($error != '') {
+              echo "<div style='padding:4px; border:1px solid red; color:red'>" . $error
+              . "</div>";
+ } ?>
 
 <form action="" method="post">
 <div>
 <?php if ($id != '') { ?>
 <input type="hidden" name="id" value="<?php echo $id; ?>" />
 <p>ID: <?php echo $id; ?></p>
-<?php } ?>
+<?php 
+                      
+} ?>
 
 <strong>First Name: *</strong> <input type="text" name="firstname"
 value="<?php echo $first; ?>"/><br/>
@@ -41,7 +43,9 @@ value="<?php echo $pos; ?>"/><br/r>
 <strong>Shirt #:  </strong> <input type="number" name="shirt"
 value="<?php echo $shirt; ?>"/>
 <p>* required</p>
+
 <input type="submit" name="submit" value="Submit" />
+
 </div>
 </form>
 </body>
@@ -59,33 +63,34 @@ EDIT RECORD
 // if the 'id' variable is set in the URL, we know that we need to edit a record
 if (isset($_GET['id']))
 {
-// if the form's submit button is clicked, we need to process the form
-if (isset($_POST['submit']))
+    // if the form's submit button is clicked, we need to process the form
+    if (isset($_POST['submit']))
 {
-// make sure the 'id' in the URL is valid
+  // make sure the 'id' in the URL is valid
 if (is_numeric($_POST['id']))
 {
-// get variables from the URL/form
-$id = $_POST['id'];
-$firstname = htmlentities($_POST['firstname'], ENT_QUOTES);
-$lastname = htmlentities($_POST['lastname'], ENT_QUOTES);
-$position = htmlentities($_POST['position'], ENT_QUOTES);
-$shirt = htmlentities($_POST['shirt'], ENT_QUOTES);
+    // get variables from the URL/form
+    $id = $_POST['id'];
+    $firstname = htmlentities($_POST['firstname'], ENT_QUOTES);
+    $lastname = htmlentities($_POST['lastname'], ENT_QUOTES);
+    $position = htmlentities($_POST['position'], ENT_QUOTES);
+    $shirt = htmlentities($_POST['shirt'], ENT_QUOTES);
 
 // check that firstname and lastname are both not empty
 if ($firstname == '' || $lastname == '')
 {
-// if they are empty, show an error message and display the form
-$error = 'ERROR: Please fill in all required fields!';
-renderForm($firstname, $lastname, $pos, $shirt, $error, $id);
+    // if they are empty, show an error message and display the form
+    $error = 'ERROR: Please fill in all required fields!';
+    renderForm($firstname, $lastname, $pos, $shirt, $error, $id);
 }
 else
 {
-// if everything is fine, update the record in the database
-if ($stmt = $mysqli->prepare("UPDATE players SET firstname = ?, lastname = ?,
-position = ?, shirt = ?
-WHERE id=?"))
+    // if everything is fine, update the record in the database
+    if ($stmt = $mysqli->prepare("UPDATE players SET firstname = ?, lastname = ?,
+    position = ?, shirt = ?
+    WHERE id=?"))
 {
+
 $stmt->bind_param("ssi", $firstname, $lastname, $position, $shirt, $id);
 $stmt->execute();
 $stmt->close();
@@ -93,52 +98,52 @@ $stmt->close();
 // show an error message if the query has an error
 else
 {
-echo "ERROR: could not prepare SQL statement.";
+    echo "ERROR: could not prepare SQL statement.";
 }
 
 // redirect the user once the form is updated
-header("Location: view.php");
+    header("Location: view.php");
 }
 }
 // if the 'id' variable is not valid, show an error message
 else
 {
-echo "Error!";
+        echo "Error!";
 }
 }
 // if the form hasn't been submitted yet, get the info from the database and show the form
 else
 {
-// make sure the 'id' value is valid
-if (is_numeric($_GET['id']) && $_GET['id'] > 0)
-{
-// get 'id' from URL
-$id = $_GET['id'];
+      // make sure the 'id' value is valid
+      if (is_numeric($_GET['id']) && $_GET['id'] > 0)
+{ 
+      // get 'id' from URL
+      $id = $_GET['id'];
 
-// get the recod from the database
+      // get the recod from the database
 if($stmt = $mysqli->prepare("SELECT * FROM players WHERE id=?"))
 {
-$stmt->bind_param("i", $id);
-$stmt->execute();
+          $stmt->bind_param("i", $id);
+          $stmt->execute();
 
-$stmt->bind_result($id, $firstname, $lastname, $position, $shirt);
-$stmt->fetch();
+          $stmt->bind_result($id, $firstname, $lastname, $position, $shirt);
+          $stmt->fetch();
 
-// show the form
-renderForm($firstname, $lastname, $position, $shirt, NULL, $id);
+                // show the form
+        renderForm($firstname, $lastname, $position, $shirt, NULL, $id);
 
-$stmt->close();
+        $stmt->close();
 }
-// show an error if the query has an error
+            // show an error if the query has an error
 else
 {
-echo "Error: could not prepare SQL statement";
+          echo "Error: could not prepare SQL statement";
 }
 }
-// if the 'id' value is not valid, redirect the user back to the view.php page
+          // if the 'id' value is not valid, redirect the user back to the view.php page
 else
 {
-header("Location: view.php");
+          header("Location: view.php");
 }
 }
 }
@@ -153,49 +158,49 @@ NEW RECORD
 // if the 'id' variable is not set in the URL, we must be creating a new record
 else
 {
-// if the form's submit button is clicked, we need to process the form
+          // if the form's submit button is clicked, we need to process the form
 if (isset($_POST['submit']))
 {
-// get the form data
-$firstname = htmlentities($_POST['firstname'], ENT_QUOTES);
-$lastname = htmlentities($_POST['lastname'], ENT_QUOTES);
-$position = htmlentities($_POST['position'], ENT_QUOTES);
-$shirt = htmlentities($_POST['shirt'], ENT_QUOTES);
+          // get the form data
+        $firstname = htmlentities($_POST['firstname'], ENT_QUOTES);
+        $lastname = htmlentities($_POST['lastname'], ENT_QUOTES);
+        $position = htmlentities($_POST['position'], ENT_QUOTES);
+        $shirt = htmlentities($_POST['shirt'], ENT_QUOTES);
 
 // check that firstname and lastname are both not empty
 if ($firstname == '' || $lastname == '')
 {
-// if they are empty, show an error message and display the form
-$error = 'ERROR: Please fill in all required fields!';
-renderForm($firstname, $lastname, $pos, $shirt, $error);
+        // if they are empty, show an error message and display the form
+        $error = 'ERROR: Please fill in all required fields!';
+        renderForm($firstname, $lastname, $pos, $shirt, $error);
 }
 else
 {
-// insert the new record into the database
+        // insert the new record into the database
 if ($stmt = $mysqli->prepare("INSERT players (firstname, lastname, position, shirt) VALUES (?, ?, ?, ?)"))
 {
-$stmt->bind_param("ss", $firstname, $lastname, $position, $shirt);
-$stmt->execute();
-$stmt->close();
+        $stmt->bind_param("ss", $firstname, $lastname, $position, $shirt);
+        $stmt->execute();
+        $stmt->close();
 }
 // show an error if the query has an error
 else
 {
-echo "ERROR: Could not prepare SQL statement.";
+        echo "ERROR: Could not prepare SQL statement.";
 }
 
 // redirec the user
-header("Location: view.php");
+        header("Location: view.php");
 }
 
 }
-// if the form hasn't been submitted yet, show the form
+        // if the form hasn't been submitted yet, show the form
 else
 {
-renderForm();
+        renderForm();
 }
 }
 
 // close the mysqli connection
-$mysqli->close();
+          $mysqli->close();
 ?>
